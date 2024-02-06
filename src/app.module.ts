14ed1c944fcc6a4ed4fadc85c1c2346612ca2e6b
lishuo <2449906403@qq.com>
2024-02-06 15:10:30 +0800

    feat: AOP架构

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,22 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonModule } from './person/person.module';
 import { OtherModule } from './other/other.module';
 import { MontedAModule } from './monted-a/monted-a.module';
 import { MontedBModule } from './monted-b/monted-b.module';
+import { LogMiddleware } from './log.middleware';
 
 @Module({
   imports: [PersonModule, OtherModule, MontedBModule, MontedAModule ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
+export class AppModule implements NestModule {
+
+  // 启用路由中间件 * 代表所有路由
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LogMiddleware).forRoutes('*');
+  }
 
 }
--- src/log.middleware.ts
+++ src/log.middleware.ts
@@ -1,8 +1,15 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
 
+// 路由中间件
 @Injectable()
 export class LogMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+  use(req: Request, res: Response, next: () => void) {
+    console.log('before2', req.url);
+
     next();
+
+    console.log('after2');
   }
 }
+
